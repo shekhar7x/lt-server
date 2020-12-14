f50b16f0ef375f13057840a5d5c115b21b420e50
shekhar7x <shekhar7x@gmail.com>
2020-12-14 04:14:28 +0000

    doneh

--- server.js
+++ server.js
@@ -50,8 +50,8 @@ export default function(opt) {
         };
     });
 
-    app.use(router.routes());
-    app.use(router.allowedMethods());
+    app.use('/localtunnel',router.routes());
+    app.use('/localtunnel',router.allowedMethods());
 
     // root endpoint
     app.use(async (ctx, next) => {
